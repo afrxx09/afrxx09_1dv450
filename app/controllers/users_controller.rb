2101efff813289c6d64129768692cd6dca5c1023
Andreas Fridlund <afrxx09@student.lnu.se>
2015-02-03 10:22:44 +0100

    form error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 	end
 	def create
 		@user = User.new(user_params)
-		@user.api_key = Digest::MD5::hexdigest(@user.email.downcase + Time.now.to_i.to_s);
+		#@user.api_key = Digest::MD5::hexdigest(@user.email.downcase + Time.now.to_i.to_s);
 		if @user.save
 			flash[:success] = "Successfully signed up!"
 			redirect_to @user
